9cdc0b8065c06c4a6d39b203c57139897cfe3c3c
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-01 08:49:28 +0700

    Remove message useless

--- lib/main.dart
+++ lib/main.dart
@@ -60,9 +60,6 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
               '$_batteryLevel',
               style: Theme.of(context).textTheme.display1,
             ),
