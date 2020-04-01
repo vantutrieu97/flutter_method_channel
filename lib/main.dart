3d4889a3dcb5c422c9ccf3a15e36d8fc6b7e7bcc
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-01 08:48:16 +0700

    Demo method channel

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'Flutter method channel - vantutrieu97'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platform = const MethodChannel('samples.flutter.dev/battery');
+  int _counter = 0;
+
+  String _batteryLevel = 'Unknown battery level.';
+
+  Future<void> _getBatteryLevel() async {
+    print('<> _getBatteryLevel :<>--------<> ');
+    String batteryLevel;
+    try {
+      print('<> trye :<>--------<> ');
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      print('<> batterrLevel :<>--------<> $result');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      print('<> catch :<>--------<> $e');
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_batteryLevel',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getBatteryLevel,
+        child: Icon(Icons.star),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
